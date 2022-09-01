19c96412a955bb8375f75e8f0f9247ed9bb27b72
itsilkerse <itsilkerse@gmail.com>
2022-09-01 10:13:19 +0200

    FizzBuzz code little update

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,11 +5,27 @@ public class FizzBuzz {
     }
     public static String fizzBuzz(int number) {
 
-        if (number % 3 == 0 && number % 5 == 0) {
+        boolean isDividableBy3And5 = number % 3 == 0 && number % 5 == 0;
+        boolean isDividableBy3 = number % 3 == 0;
+        boolean isDividableBy5 = number % 5 == 0;
+
+        /*
+        3 / 3 = 1, Rest 0 = True
+        4 / 3 = 1, Rest 1 = False
+        5 / 3 = 1, Rest 2 = False
+        5 / 3 = 2, Rest 0 = True
+        If Rest = 0 = True
+        If Rest = 0 + X = False
+         */
+
+
+        if (isDividableBy3And5) {
             return "fizzbuzz";
-        } else if (number % 3 == 0) {
+
+        } else if (isDividableBy3) {
             return "fizz";
-        } else if (number % 5 == 0) {
+
+        } else if (isDividableBy5) {
             return "buzz";
         }
             return String.valueOf(number);
---
+++ src/main/java/greetings.java
@@ -0,0 +1,16 @@
+public class greetings {
+    public static void main(String[] args) {
+
+        greetings("ilker");
+        greetings("Nina");
+        greetings("Joon");
+
+    }
+
+
+    public static void greetings(String Name) {
+        System.out.println ("Hallo, " + Name + " wie geht es dir heute");
+
+    }
+
+}
